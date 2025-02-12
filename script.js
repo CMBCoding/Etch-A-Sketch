e028f0ffe02aa358583525fbeb67d79f1288fe83
Corey Brown <189717925+CMBCoding@users.noreply.github.com>
2025-02-12 16:37:32 +0900

    Fill out grid using DOM and a loop. Create event listener for sketch feature.

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-console.log("Hello");
 const screen = document.querySelector("#frame-inner");
 const block = document.createElement("div");
 
 /*Created block for sketch grid and stylings*/
-block.innerText = "Box";
 block.style.backgroundColor = "gainsboro";
 block.style.display = "flex";
-block.style.height = "50px";
-block.style.width = "50px";
-block.style.alignItems = "center";
-block.style.justifyContent = "center";
+block.style.flexBasis = "50px";
+// block.style.height = "50px";
+// block.style.width = "50px";
 
-screen.appendChild(block);
+// screen.appendChild(block);
 let blocks = block.cloneNode(true);
-screen.appendChild(blocks);
-screen.appendChild(blocks);
+blocks.addEventListener("mouseover", () =>
+    blocks.style.backgroundColor = "grey"
+);
+
+for (let i = 0; i < 256; i++) {
+    const blocks = block.cloneNode(true);
+    screen.appendChild(blocks);
+    blocks.addEventListener("mouseover", () =>
+        blocks.style.backgroundColor = "grey"
+    );
+}
