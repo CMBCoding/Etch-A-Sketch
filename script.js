50535969540ef4c819ee3d3d6af2fd60b371523e
CMBCoding <189717925+CMBCoding@users.noreply.github.com>
2025-02-17 21:14:59 +0900

    Create clear screen function. Turn dials into buttons for clearning scree. Edit html and css.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const screen = document.querySelector("#frame-inner");
+const frame = document.querySelector("#frame-inner");
 const block = document.createElement("div");
 
 /*Created block for sketch grid and stylings*/
@@ -15,7 +15,45 @@ blocks.addEventListener("mouseover", () =>
 
 for (let i = 0; i < 256; i++) {
     const blocks = block.cloneNode(true);
-    screen.appendChild(blocks);
+    frame.appendChild(blocks);
+    blocks.addEventListener("mouseover", () =>
+        blocks.style.backgroundColor = "grey"
+    );
+}
+
+const leftWheel = document.querySelector("#wheel-left");
+const rightWheel = document.querySelector("#wheel-right");
+
+
+leftWheel.addEventListener("click", changeScreen);
+rightWheel.addEventListener("click", clearScreen);
+let size;
+
+//Find way to convert size to a pixel value;
+function changeScreen() {
+    clearScreen();
+    area = prompt("Enter a number to generate the area for your new grid");
+    let size = Number(area);
+    frame.style.width = size;
+    frame.style.height = size;
+    return frame.style.width, frame.style.height;
+    // for (let i = 0; i < area; i++) {
+    //     drawScreen();
+    // }
+};
+
+/* Function for removal of nodes from the frame to create a blank frame.
+Removes each firstChild from the frame until there are none left. */
+function clearScreen() {
+    while (frame.firstChild) {
+        frame.removeChild(frame.firstChild)
+    }
+};
+
+//Test for adding blocks. Cut//
+function drawScreen() {
+    const blocks = block.cloneNode(true);
+    frame.appendChild(blocks);
     blocks.addEventListener("mouseover", () =>
         blocks.style.backgroundColor = "grey"
     );
