8ad62c37f9d59090ee087b5adf48e3def55b8118
Corey Brown <189717925+CMBCoding@users.noreply.github.com>
2025-02-20 11:10:15 +0900

    Fix grid reset function. Next fix equal sizing for flex items upon genertion

--- script.js
+++ script.js
@@ -21,94 +21,39 @@ for (let i = 0; i < 16; i++) {
 
 const leftWheel = document.querySelector("#wheel-left");
 const rightWheel = document.querySelector("#wheel-right");
-
-
 leftWheel.addEventListener("click", changeScreen);
 
 function changeScreen() {
-    gridX = prompt("Enter a number from 1 to 100 for the width of your new grid.");
-    gridY = prompt("Enter a number from 1 to 100 for the height of your new grid.");
-    if (gridY <= 100 && gridX <= 100) {
-        console.log(gridY);
-        console.log(gridX);
-        let gridArea = gridY * gridX;
-        console.log(gridArea);
+    grid = prompt("Enter a number from 1 to 100 for the area of your new grid.");
+    if (grid <= 100) {
         clearScreen();
-        drawGrid(gridX, gridY);
+        drawGrid(grid);
     } else {
-        alert("Please enter a number between 1 and 100 for your grid length and width.")
+        alert("Please enter a number from 1 to 100 for the area of your new grid.")
     }
 };
 
 function drawGrid() {
-    // let gridArea = gridY * gridX;
-    drawColumns(gridY);
-    // for (i = 0; i < gridArea; i++) {
-    //     let blocks = block.cloneNode(true);
-    //     frame.appendChild(blocks);
-    //     blocks.addEventListener("mouseover", () =>
-    //     blocks.style.backgroundColor = "grey");
-    // }
-};
-
-function drawColumns() {
-    for (let i = 0; i < gridX; i++) {
+    let gridArea = grid * grid;
+    console.log(gridArea);
+    for (let i = 0; i < (gridArea / 2); i++) {
         let columns = column.cloneNode(true);
-        let blocks = block.cloneNode(true);
-        // let columnsArray = Array.from(columns);
         frame.appendChild(columns);
-        columns.addEventListener("mouseover", () => 
-        columns.style.backgroundColor = "gray")
-        // columnsArray.forEach(columns => { 
-        //     columns.appendChild(blocks/gridY);
-        // });
-    }
+        for (let i = 0; i < grid; i++) {
+            let blocks = block.cloneNode(true);
+            columns.appendChild(blocks);
+            blocks.addEventListener("mouseover", () =>
+            blocks.style.backgroundColor = "grey"
+            );
+        }
+    };
 };
-
-// function drawColumns() {
-//     for (let i = 0; i < gridX; i++) {
-//         let columns = column.cloneNode(true);
-//         // columns.style.width = (100 / gridX);
-//         frame.appendChild(columns);
-//     }
-// };
-
-// function drawBlocks() {
-//     for (let i = 0; i < gridY; i ++) {
-//         let blocks = block.cloneNode(true);
-//         columns.appendChild(blocks);
-//     }
-// }
 
 function clearScreen() {
     while (frame.firstChild) {
         frame.removeChild(frame.firstChild)
     }
 };
-
-// function drawScreen() {
-//     for (let i = 0; i < 16; i++) {
-//         let columns = column.cloneNode(true);
-//         frame.appendChild(columns);
-//         if ((area % 2) === 0 ) {}
-//         for (let i = 0; i < area; i++) {
-//         const blocks = block.cloneNode(true);
-//         column.appendChild(blocks);
-//         blocks.addEventListener("mouseover", () =>
-//         blocks.style.backgroundColor = "grey");
-//         }
-//     };
-// }
-
-//Old drawScreen
-// function drawScreen() {      
-//         for (let i = 0; i < area; i++) {
-//         const blocks = block.cloneNode(true);
-//         frame.appendChild(blocks);
-//         blocks.addEventListener("mouseover", () =>
-//         blocks.style.backgroundColor = "grey"
-//     );}
-// }
 
 // let red;
 // let blue;
